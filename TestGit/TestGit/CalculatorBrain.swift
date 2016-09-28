a6db1aea2aac0698c575b2048a798033c9516ea0
Can <504461918@qq.com>
2016-09-29 03:48:01 +1000

    calculator

---
+++ TestGit/TestGit/CalculatorBrain.swift
@@ -0,0 +1,86 @@
+//
+//  CalculatorBrain.swift
+//  Calcu
+//
+//  Created by 灿 崔 on 27/09/2016.
+//  Copyright © 2016 Can. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double){
+        accumulator = operand
+    }
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double,Double) -> Double)
+        case Reset
+        case Equals
+    }
+    
+    var operations: Dictionary<String,Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "sin": Operation.UnaryOperation(sin),
+        "=" : Operation.Equals,
+        "+" : Operation.BinaryOperation(+),
+        "−" : Operation.BinaryOperation(-),
+        "×" : Operation.BinaryOperation(*),
+        "÷" : Operation.BinaryOperation(/),
+        "AC" : Operation.Reset
+        
+    ]
+    
+    
+    func performOperation(symbol: String){
+        if let operation = operations[symbol]{
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = pendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals :
+                executePendingBinaryOperation()
+            case .Reset:
+                pending = nil
+                accumulator = 0
+                
+            }
+        }
+        
+    }
+    private func executePendingBinaryOperation(){
+        if pending != nil{
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending:pendingBinaryOperationInfo?
+    
+    struct pendingBinaryOperationInfo {
+        var binaryFunction: (Double,Double) -> Double
+        var firstOperand: Double
+        
+    }
+    
+    
+    
+    var result: Double{
+        get {
+            return accumulator
+        }
+    }
+    
+}
--- TestGit/TestGit/ViewController.swift
+++ TestGit/TestGit/ViewController.swift
@@ -1,25 +1,58 @@
 //
 //  ViewController.swift
-//  TestGit
+//  Calcu
 //
-//  Created by 灿 崔 on 29/09/2016.
+//  Created by 灿 崔 on 26/09/2016.
 //  Copyright © 2016 Can. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    private var brain = CalculatorBrain()
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping: Bool = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        print("tough \(digit)")
+        if userIsInTheMiddleOfTyping{
+            let digitCurrentlyInDisplay = display!.text!
+            display.text = digitCurrentlyInDisplay + digit
+        }else{
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var displayValue: Double {
+        get{
+            return Double(display.text!)!
+        }
+        
+        set{
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping{
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        
+        if let mathematicalOperation = sender.currentTitle{
+            brain.performOperation(mathematicalOperation)
+        }
+        displayValue = brain.result
+    }
 }
 
